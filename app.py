fbad5830cec9007c9ce7ec4a82fd0fc53a8c3e04
Tamiro2019 <tamiro.villazon@gmail.com>
2020-01-17 19:28:14 -0500

    modify mathjax url

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # setup mathjax to interpret Tex 
-mathjax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML'
+mathjax = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML'
 app.scripts.append_script({ 'external_url' : mathjax })
 
 ## Design page layout ##    
